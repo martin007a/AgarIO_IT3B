4d259d00c7a8d30ea002e2db6bd27a3a18b0456d
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2024-02-19 10:41:31 +0100

    Přidejte soubory projektu.

---
+++ AgarIO_IT3B/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AgarIO_IT3B
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+  }
+
+}
---
+++ AgarIO_IT3B/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace AgarIO_IT3B
+{
+  public class Player
+  {
+    public double Size { get; set; }
+    public double Speed { get; set; }
+    public Brush Color { get; set; }
+    public string Name { get; set; }
+
+    public Player(Brush color, string name) 
+    {
+      Color = color;
+      Name = name;
+      Size = 0;
+    }
+  }
+}
