60a2d50554ef806ff104adf5f3cd60ed49c347c0
Martin Slavíček <slaviekmartin@post.cz>
2024-03-25 09:40:12 +0100

    9:40

--- AgarIO_IT3B/MainWindow.xaml.cs
+++ AgarIO_IT3B/MainWindow.xaml.cs
@@ -18,7 +18,32 @@ namespace AgarIO_IT3B
   {
     public MainWindow()
     {
-      InitializeComponent();
+            
     }
-  }
+
+    public void See()
+    {
+        double y = canvasGame.ActualHeight / 2;
+        double x = canvasGame.ActualWidth / 2;
+
+        Player plar = new Player(Brushes.Red, "Arnold") 
+        { 
+            Location = new Point(x, y),
+        };    
+        Ellipse eli = new Ellipse() 
+        {
+            Fill = plar.Color,
+            Width = plar.Size,
+            Height = plar.Size,
+        };
+        canvasGame.Children.Add(eli);
+        Canvas.SetLeft(eli, plar.Location.X);
+        Canvas.SetTop(eli, plar.Location.Y);
+    }
+
+        private void canvasGame_Loaded(object sender, RoutedEventArgs e)
+        {
+            See();
+        }
+    }
 }
