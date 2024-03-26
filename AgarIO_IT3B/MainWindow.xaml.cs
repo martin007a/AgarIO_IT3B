3f9efee6eadee06f82d459c66357c8cefddcea15
martin007a <90248875+martin007a@users.noreply.github.com>
2024-03-26 19:47:06 +0100

    jako že snaha byla

--- AgarIO_IT3B/MainWindow.xaml.cs
+++ AgarIO_IT3B/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace AgarIO_IT3B
 {
@@ -18,14 +20,60 @@ namespace AgarIO_IT3B
   {
         double y;
         double x;
+        double speedX;
+        double speedY;
         Ellipse eli = new Ellipse();
+        private DispatcherTimer dispatcherTimer;
+        private double MouseX;
+        private double MouseY;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += canvasGame_Loaded;
+        }
 
-    public MainWindow()
-    {
+        private void Timer()
+        {
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(5);
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Start();
+        }
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            y = Canvas.GetTop(eli);
+            x = Canvas.GetLeft(eli);
+            if (x != MouseX)
+            {
+                speedX = x - MouseX /10; 
+                x += -speedX;
+            }
+            if (x < MouseX)
+            {
+                x = MouseX;
+            }
+            if (y != MouseY)
+            {
+                speedY = y - MouseY /10;
+                y += -speedY;
+            }
+            if (y < MouseY)
+            {
+                y = MouseY;
+            }
+            else
+            {
+                dispatcherTimer.Stop();
+            }
+            Lbl.Content = speedX.ToString() + "\n";
+            Lbl.Content += speedY.ToString() + "\n";
+            Canvas.SetLeft(eli, x);
+            Canvas.SetTop(eli, y);
+        }
 
-    }
         public void See()
         {
+            canvasGame.Children.Clear();
             double y = canvasGame.ActualHeight / 2;
             double x = canvasGame.ActualWidth / 2;
             Player plar = new Player(Brushes.Red, "Arnold") 
@@ -41,8 +89,8 @@ namespace AgarIO_IT3B
             canvasGame.Children.Add(eli);
             Canvas.SetLeft(eli, plar.Location.X);
             Canvas.SetTop(eli, plar.Location.Y);
+
         }
-    
 
         private void canvasGame_Loaded(object sender, RoutedEventArgs e)
         {
@@ -51,7 +99,7 @@ namespace AgarIO_IT3B
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            x = Canvas.GetLeft(eli);
+            /*x = Canvas.GetLeft(eli);
             y = Canvas.GetTop(eli);
 
 
@@ -75,8 +123,24 @@ namespace AgarIO_IT3B
 
             Lbl.Content = x.ToString();
             Lbl.Content += "\n";
-            Lbl.Content += y.ToString();
+            Lbl.Content += y.ToString();*/
 
+        }
+
+        private void canvasGame_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            Point Position = Mouse.GetPosition(this);
+            MouseX = Position.X;
+            MouseY = Position.Y;
+
+           /* Lbl.Content = MouseX.ToString() + "\n";
+            Lbl.Content += MouseY.ToString() + "\n";
+            Lbl.Content += "Ahoj";*/
+
+            if((MouseX != x) || (MouseY != y)) 
+            {
+                Timer();
+            }
         }
     }
 }
